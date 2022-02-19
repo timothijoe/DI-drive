5e7e4710e964b7d47fa901b94da86b4544bd9294
timothijoe <zt1301112@gmail.com>
2022-02-20 02:37:55 +0800

    add other scenario

--- demo/metadrive/now/try_new/z1_exp3_ppo_intersection.py
+++ demo/metadrive/now/try_new/z1_exp3_ppo_intersection.py
@@ -6,36 +6,33 @@ from tensorboardX import SummaryWriter
 from metadrive import TopDownMetaDrive
 from ding.envs import BaseEnvManager, SyncSubprocessEnvManager
 from ding.config import compile_config
-from ding.policy import SACPolicy
-from ding.worker import SampleSerialCollector, InteractionSerialEvaluator, BaseLearner, NaiveReplayBuffer
+from ding.policy import PPOPolicy
+from ding.worker import SampleSerialCollector, InteractionSerialEvaluator, BaseLearner
 from core.envs import DriveEnvWrapper
 from core.policy.ad_policy.conv_vac import ConvVAC
 from core.envs.md_hrl_env import MetaDriveHRLEnv
-from ding.policy import PPOPolicy
 from core.utils.simulator_utils.md_utils.traffic_manager_utils import TrafficMode
 
 metadrive_basic_config = dict(
-    exp_name='metadrive_tdv_ppo',
+    exp_name='ppo_inter',
     env=dict(
         metadrive=dict(
-        use_render=True,
-        show_seq_traj=True,
-        use_jerk_penalty = True,
-        use_lateral_penalty = True,
-        traffic_density = 0.0,
-        seq_traj_len = 10,
-        map='SOSO', 
-        traffic_mode=TrafficMode.Trigger,
+            use_render=False,
+            use_jerk_penalty = True,
+            use_lateral_penalty = False,
+            traffic_density = 0.3,
+            map='XSXS', 
+            #traffic_mode=TrafficMode.Trigger,
         ),
         manager=dict(
             shared_memory=False,
             max_retry=2,
             context='spawn',
         ),
-        n_evaluator_episode=1,
+        n_evaluator_episode=6,
         stop_value=99999,
-        collector_env_num=1,
-        evaluator_env_num=1,
+        collector_env_num=11,
+        evaluator_env_num=3,
     ),
     policy=dict(
         cuda=True,
@@ -80,29 +77,42 @@ def main(cfg):
     )
 
     collector_env_num, evaluator_env_num = cfg.env.collector_env_num, cfg.env.evaluator_env_num
-    evaluator_env = SyncSubprocessEnvManager(
+    collector_env = SyncSubprocessEnvManager(
         env_fn=[partial(wrapped_env, cfg.env.metadrive) for _ in range(collector_env_num)],
         cfg=cfg.env.manager,
     )
-    # evaluator_env = SyncSubprocessEnvManager(
-    #     env_fn=[partial(wrapped_env, cfg.env.metadrive) for _ in range(evaluator_env_num)],
-    #     cfg=cfg.env.manager,
-    # )
+    evaluator_env = SyncSubprocessEnvManager(
+        env_fn=[partial(wrapped_env, cfg.env.metadrive) for _ in range(evaluator_env_num)],
+        cfg=cfg.env.manager,
+    )
 
     model = ConvVAC(**cfg.policy.model)
     policy = PPOPolicy(cfg.policy, model=model)
 
-
-    import torch
-    policy._load_state_dict_collect(torch.load('/home/SENSETIME/zhoutong/hoffnung/xad/iteration_ckpt/feb19/exp3/ppo_only_jerk_len_10.pth.tar', map_location = 'cpu'))
-
-
     tb_logger = SummaryWriter('./log/{}/'.format(cfg.exp_name))
-    #learner = BaseLearner(cfg.policy.learn.learner, policy.learn_mode, tb_logger, exp_name=cfg.exp_name)
-    evaluator = InteractionSerialEvaluator(cfg.policy.eval.evaluator, evaluator_env, policy.eval_mode, tb_logger, exp_name=cfg.exp_name)
-    for iter in range(5):
-        stop, reward = evaluator.eval()
+    learner = BaseLearner(cfg.policy.learn.learner, policy.learn_mode, tb_logger, exp_name=cfg.exp_name)
+    collector = SampleSerialCollector(
+        cfg.policy.collect.collector, collector_env, policy.collect_mode, tb_logger, exp_name=cfg.exp_name
+    )
+    evaluator = InteractionSerialEvaluator(
+        cfg.policy.eval.evaluator, evaluator_env, policy.eval_mode, tb_logger, exp_name=cfg.exp_name
+    )
+
+    learner.call_hook('before_run')
+
+    while True:
+        if evaluator.should_eval(learner.train_iter):
+            stop, rate = evaluator.eval(learner.save_checkpoint, learner.train_iter, collector.envstep)
+            if stop:
+                break
+        # Sampling data from environments
+        new_data = collector.collect(cfg.policy.collect.n_sample, train_iter=learner.train_iter)
+        learner.train(new_data, collector.envstep)
+    learner.call_hook('after_run')
+
+    collector.close()
     evaluator.close()
+    learner.close()
 
 
 if __name__ == '__main__':
